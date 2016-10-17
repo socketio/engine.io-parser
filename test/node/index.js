9cb36b0bd411d7a3df3cd7df59d4b71f3978d9af
Rainforce15 <rainforce15@gmail.com>
2016-10-17 11:51:34 +0200

    fixed ArrayBuffer to Buffer conversion for 0.12 compatibility + test

--- test/node/index.js
+++ test/node/index.js
@@ -117,24 +117,31 @@ describe('parser', function() {
   });
 
   it('should decode an ArrayBuffer/Buffer with undeclared type as binary', function(done) {
-    var buffer = new ArrayBuffer(4);
-    var dataview = new DataView(buffer);
-    for (var i = 0; i < buffer.byteLength ; i++) dataview.setInt8(i, 4-i);
+    var buffer1 = new ArrayBuffer(4);
+    var dataview = new DataView(buffer1);
+    var buffer2 = new Buffer(4);
+    for (var i = 0; i < buffer1.byteLength ; i++) {
+      dataview.setInt8(i, 4-i);
+      buffer2[i] = 4-i;
+    }
 
-    var decoded1 = decode(buffer, null);
-    var decoded2 = decode(new Buffer(buffer), null);
-    expect(decoded1).to.eql({ type: 'message', data: new Buffer(buffer.slice(1))});
-    expect(new Uint8Array(decoded1.data)).to.eql(new Uint8Array(buffer.slice(1)));
-    expect(decoded2).to.eql({ type: 'message', data: new Buffer(buffer.slice(1))});
-    expect(new Uint8Array(decoded2.data)).to.eql(new Uint8Array(buffer.slice(1)));
+    var decoded1 = decode(buffer1, null);
+    var decoded2 = decode(buffer2, null);
+    expect(decoded1).to.eql({ type: 'message', data: new Buffer([3,2,1])});
+    expect(new Uint8Array(decoded1.data)).to.eql(new Uint8Array(buffer1.slice(1)));
+    expect(decoded2).to.eql({ type: 'message', data: new Buffer(buffer2.slice(1))});
+    expect(new Uint8Array(decoded2.data)).to.eql(new Uint8Array(buffer2.slice(1)));
     done();
   });
 
   it('should decode an ArrayBuffer/Buffer as binary of type ArrayBuffer', function(done) {
     var buffer1 = new ArrayBuffer(4);
     var dataview = new DataView(buffer1);
-    for (var i = 0; i < buffer1.byteLength ; i++) dataview.setInt8(i, 4-i);
-    var buffer2 = new Buffer(buffer1);
+    var buffer2 = new Buffer(4);
+    for (var i = 0; i < buffer1.byteLength ; i++) {
+      dataview.setInt8(i, 4-i);
+      buffer2[i] = 4-i;
+    }
 
     var decoded1 = decode(buffer1, 'arraybuffer');
     var decoded2 = decode(buffer2, 'arraybuffer');
