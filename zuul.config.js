1b7866d379dad90b687e6d5213ff7a9634be29e5
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2016-12-21 08:46:09 +0100

    Use external browsers list

--- zuul.config.js
+++ zuul.config.js
@@ -1,13 +1,13 @@
 'use strict';
 
+var browsers = require('socket.io-browsers');
+
 var zuulConfig = module.exports = {
-  tunnel: {
-    type: 'localtunnel'
-  },
   ui: 'mocha-bdd',
 
-  // only used when run with saucelabs
-  // not activated when dev or phantom
+  // test on localhost by default
+  local: true,
+
   concurrency: 2, // ngrok only accepts two tunnels by default
   // if browser does not sends output in 120s since last output:
   // stop testing, something is wrong
@@ -15,37 +15,15 @@ var zuulConfig = module.exports = {
   browser_open_timeout: 60 * 4 * 1000,
   // we want to be notified something is wrong asap, so no retry
   browser_retries: 1,
-
-  browsers: [{
-    name: 'chrome',
-    version: 'latest'
-  }, {
-    name: 'firefox',
-    version: 'latest'
-  }, {
-    name: 'internet explorer',
-    version: '6..10'
-  }, {
-    name: 'safari',
-    version: 'latest'
-  }, {
-    name: 'iphone',
-    version: '-3..latest'
-  }, {
-    name: 'android',
-    version: '-3..latest'
-  }, {
-    name: 'ipad',
-    version: 'latest'
-  }, {
-    name: 'microsoftedge',
-    version: 'latest'
-  }]
 };
 
 if (process.env.CI === 'true') {
+  zuulConfig.local = false;
   zuulConfig.tunnel = {
     type: 'ngrok',
     bind_tls: true
   };
 }
+
+var isPullRequest = process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST !== 'false';
+zuulConfig.browsers = isPullRequest ? browsers.pullRequest : browsers.all;
